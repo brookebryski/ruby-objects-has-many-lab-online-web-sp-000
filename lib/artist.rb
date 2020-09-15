b97bfa0e79cf795152d11d96404e969e6db0917d
chic-state-0895 <brookeabryski@gmail.com>
2020-09-15 22:38:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,12 +9,14 @@ class Artist
     @name = name
     @songs = []
   end
-
+  
 
   def add_song(song)
-    @@song_count += 1
-    @songs << song
     song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def add_song_by_name(name)
