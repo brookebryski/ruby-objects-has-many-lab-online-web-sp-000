82b7e97c36bf9b1a05cb4f9dc6afc7737e65840c
chic-state-0895 <brookeabryski@gmail.com>
2020-09-15 23:19:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,31 @@
 class Artist
-
-  attr_accessor :name, :songs
-
-
+  attr_accessor :name
+  @@song_count = 0
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
+    @songs = [] #empty collection of songs
   end
 
   def songs
-   Song.all.select {|song| song.artist == self}
- end
- 
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
 end
+ 31  lib/author.rb 
